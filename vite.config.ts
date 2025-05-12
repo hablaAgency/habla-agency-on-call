e02d13efc100fdbaa9c18b8646e5ad439aeac5f4
MacuRuiz <ruiz.martinalara@gmail.com>
2025-05-12 11:49:51 -0300

    fix node

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,11 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { componentTagger } from "lovable-tagger";
+import { webcrypto as crypto } from 'node:crypto'
+
+if (!globalThis.crypto) {
+  globalThis.crypto = crypto as unknown as Crypto
+}
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
